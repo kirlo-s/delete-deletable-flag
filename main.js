6baae9c631691fb2284a141463e87f5859f52085
kirlo_s <kirlo.stnu@gmail.com>
2022-10-09 18:26:12 +0900

    1.1.0

--- main.js
+++ main.js
@@ -2,42 +2,44 @@ const deletable = (function () {
 
     const pluginName = "portal-editable";
 
-    const deletableRegister = (function (){
-        function precondition() {
-            return "enabled";
-        }
-
-        function callback() {
-            const parser = new window.DOMParser();
-
-            const workspace = _Blockly.getMainWorkspace();
-            const workspaceData = _Blockly.Xml.domToText(_Blockly.Xml.workspaceToDom(workspace, true));
-            
-            const xmlData = parser.parseFromString( workspaceData , "text/xml");
-            const d = xmlData.querySelectorAll('[deletable]');
-
-            for(i of d){
-                i.removeAttribute('deletable');
-                i.removeAttribute('movable');
-                i.removeAttribute('editable');
+    /*
+        const deletableRegister = (function (){
+            function precondition() {
+                return "enabled";
             }
-            var s = new XMLSerializer();
-            var xmlStr = s.serializeToString(xmlData);
 
-            _Blockly.getMainWorkspace().clear();
-            _Blockly.Xml.domToWorkspace(_Blockly.Xml.textToDom(xmlStr), workspace);
-        }
+            function callback() {
+                const parser = new window.DOMParser();
 
-        return {
-            id: "disable-write-protect",
-            displayText: "Enable Edit",
-            // eslint-disable-next-line no-undef
-            scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
-            weight: 99,
-            preconditionFn: precondition,
-            callback: callback
-        };
-    })();
+                const workspace = _Blockly.getMainWorkspace();
+                const workspaceData = _Blockly.Xml.domToText(_Blockly.Xml.workspaceToDom(workspace, true));
+                
+                const xmlData = parser.parseFromString( workspaceData , "text/xml");
+                const d = xmlData.querySelectorAll('[deletable]');
+
+                for(i of d){
+                    i.removeAttribute('deletable');
+                    i.removeAttribute('movable');
+                    i.removeAttribute('editable');
+                }
+                var s = new XMLSerializer();
+                var xmlStr = s.serializeToString(xmlData);
+
+                _Blockly.getMainWorkspace().clear();
+                _Blockly.Xml.domToWorkspace(_Blockly.Xml.textToDom(xmlStr), workspace);
+            }
+
+            return {
+                id: "disable-write-protect",
+                displayText: "Enable Edit",
+                // eslint-disable-next-line no-undef
+                scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+                weight: 99,
+                preconditionFn: precondition,
+                callback: callback
+            };
+        })();
+    */
 
     function deleteAttributes(){
         const parser = new window.DOMParser();
@@ -62,14 +64,17 @@ const deletable = (function () {
 
         _Blockly.getMainWorkspace().clear();
         _Blockly.Xml.domToWorkspace(_Blockly.Xml.textToDom(xmlStr), workspace);
-        
+
     }
 
 
     function init() {
         plugin = BF2042Portal.Plugins.getPlugin(pluginName);
 
-        _Blockly.ContextMenuRegistry.registry.register(deletableRegister);
+        /* 
+            _Blockly.ContextMenuRegistry.registry.register(deletableRegister);
+        */
+       
         deleteAttributes();
     }
 
