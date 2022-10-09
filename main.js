c5856cac6c0d9455f5f83a02ced7a2f6cfe04c1e
kirlo_s <kirlo.stnu@gmail.com>
2022-10-09 18:20:23 +0900

    1.0.3b

--- main.js
+++ main.js
@@ -39,12 +39,38 @@ const deletable = (function () {
         };
     })();
 
+    function deleteAttributes(){
+        const parser = new window.DOMParser();
+
+        const workspace = _Blockly.getMainWorkspace();
+        const workspaceData = _Blockly.Xml.domToText(_Blockly.Xml.workspaceToDom(workspace, true));
+        
+        const xmlData = parser.parseFromString( workspaceData , "text/xml");
+        const d = xmlData.querySelectorAll('[deletable]');
+        if(d.length == 0){
+            return;
+        }
+
+        for(i of d){
+            i.removeAttribute('deletable');
+            i.removeAttribute('movable');
+            i.removeAttribute('editable');
+        }
+
+        var s = new XMLSerializer();
+        var xmlStr = s.serializeToString(xmlData);
+
+        _Blockly.getMainWorkspace().clear();
+        _Blockly.Xml.domToWorkspace(_Blockly.Xml.textToDom(xmlStr), workspace);
+        
+    }
 
 
     function init() {
         plugin = BF2042Portal.Plugins.getPlugin(pluginName);
 
         _Blockly.ContextMenuRegistry.registry.register(deletableRegister);
+        deleteAttributes();
     }
 
     init();
